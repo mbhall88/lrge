0e6d65be3090600ad2b5efd83ce30b5dd9d99ca2
Michael Hall <michael@mbh.sh>
2024-11-19 12:45:45 +1000

    test: add dv rounding tests

--- liblrge/src/minimap2/mapping.rs
+++ liblrge/src/minimap2/mapping.rs
@@ -203,4 +203,68 @@ mod tests {
         let expected = "SRR28370649.1\t4402\t40\t237\t-\tSRR28370649.7311\t5094\t41\t238\t190\t197\t0\ttp:A:S\tcm:i:59\ts1:i:190\tdv:f:0.0022\trl:i:56\n";
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn test_serialize_mapping_dv_round_down() {
+        let mapping = Mapping {
+            query_name: b"SRR28370649.1".to_vec(),
+            query_len: 4402,
+            query_start: 40,
+            query_end: 237,
+            strand: '-',
+            target_name: b"SRR28370649.7311".to_vec(),
+            target_len: 5094,
+            target_start: 41,
+            target_end: 238,
+            match_len: 190,
+            block_len: 197,
+            mapq: 0,
+            tp: 'S',
+            cm: 59,
+            s1: 190,
+            dv: 0.0022111,
+            rl: 56,
+        };
+        let mut wtr = csv::WriterBuilder::new()
+            .delimiter(b'\t')
+            .has_headers(false)
+            .from_writer(vec![]);
+        wtr.serialize(mapping).unwrap();
+        let result = wtr.into_inner().unwrap();
+        let result = String::from_utf8(result).unwrap();
+        let expected = "SRR28370649.1\t4402\t40\t237\t-\tSRR28370649.7311\t5094\t41\t238\t190\t197\t0\ttp:A:S\tcm:i:59\ts1:i:190\tdv:f:0.0022\trl:i:56\n";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_serialize_mapping_dv_round_up() {
+        let mapping = Mapping {
+            query_name: b"SRR28370649.1".to_vec(),
+            query_len: 4402,
+            query_start: 40,
+            query_end: 237,
+            strand: '-',
+            target_name: b"SRR28370649.7311".to_vec(),
+            target_len: 5094,
+            target_start: 41,
+            target_end: 238,
+            match_len: 190,
+            block_len: 197,
+            mapq: 0,
+            tp: 'S',
+            cm: 59,
+            s1: 190,
+            dv: 0.0021999,
+            rl: 56,
+        };
+        let mut wtr = csv::WriterBuilder::new()
+            .delimiter(b'\t')
+            .has_headers(false)
+            .from_writer(vec![]);
+        wtr.serialize(mapping).unwrap();
+        let result = wtr.into_inner().unwrap();
+        let result = String::from_utf8(result).unwrap();
+        let expected = "SRR28370649.1\t4402\t40\t237\t-\tSRR28370649.7311\t5094\t41\t238\t190\t197\t0\ttp:A:S\tcm:i:59\ts1:i:190\tdv:f:0.0022\trl:i:56\n";
+        assert_eq!(result, expected);
+    }
 }
